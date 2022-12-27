75f0afeb09637c65eda75f75043f17f185d28d58
ALEJANDRO VAN DEN BUSSCHE <rocketmedia.es@gmail.com>
2022-12-26 19:58:22 -0500

    adelanto mokepon

--- mokepon/js/mokepon.js
+++ mokepon/js/mokepon.js
@@ -1,65 +1,177 @@
+// VARIABLES GLOBALES
 
-// FUNCIÓN PARA ESCUCHAR EVENTO CLICK AL SELECCIONAR LA MASCOTA CUANDO CLIQUEMOS EL BOTON
+let ataqueJugador
+let ataqueEnemigo
+
+//FIN VARIABLES GLOBALES
+
+//CON ESTA FUNCIÓN ESTAMOS ESCUCHANDO CUANDO ESTARÁ CARGADO TODO EL HTML
+//PUEDIENDO INICIAR EL JUEGO CON LA PRIMERA ACCION REQUERIDA PARA ESTE
+// EN ESTE CASO SERÁ HACER CLICK EN EL BOTON PARA SELECCIONAR MASCOTA
+
+
 function iniciarJuego() {
-    let botonMascotaJugador = document.getElementById("boton-mascota");
-    botonMascotaJugador.addEventListener('click', seleccionarMascotaJugador);
+    let botonMascotaJugador = document.getElementById('boton-mascota-jugador')
+    botonMascotaJugador.addEventListener('click', seleccionarMascotaJugador)
+
+//CREAMOS LAS VARIABLES PARA LOS BOTONES DE LOS ATAQUES
+//Y LO HACEMOS JUSTO DESPUÉS DEL OTRO BOTÓN, EL DE ELEGIR MASCOTA JUGADOR
+
+    let botonFuego = document.getElementById("boton-fuego")
+    botonFuego.addEventListener('click',ataqueFuego)
+    let botonAgua = document.getElementById('boton-agua')
+    botonAgua.addEventListener('click',ataqueAgua)
+    let botonTierra = document.getElementById('boton-tierra')
+    botonTierra.addEventListener('click',ataqueTierra)
+
+
 }
 
-//FUNCIÓN PARA SELECCIONAR MASCOTA Y ENVIAR MENSAJE DICIENDO QUE M,ASCOTA HE SELECCIONADO
+window.addEventListener('load', iniciarJuego)
+
+
+
+
+// CON ESTO COMIENZA EL SCRIP PARA TODO EL JUEGO: 
+//1. FUNCIÓN INICIAR JUEGO CON EL BOTON DE SELECCIONAR MASCOTA
+//2. ALERTA INDICANDO QUE HAS SELECCIONADO MASCOTA
+//3. ESCUCHANDO CUANDO CARGUE TODO EL HTML Y DARLE 'PERMISO' A LA
+//FUNCION 'INICIAR JUEGO' PARA QUE PUEDA EJECUTARSE.
+
+// ---------------------------- INICIO DEL JUEGO ------------------------------
+
+
 function seleccionarMascotaJugador() {
-    let inputHipodoge = document.getElementById('hipodoge');
-    let inputCapipepo = document.getElementById('capipepo');
-    let inputRatigueya = document.getElementById('ratigueya');
-    let inputLangostelvi = document.getElementById('langostelvi');
-    let inputTucapalma = document.getElementById('tucapalma');
-    let inputPydos = document.getElementById('pydos');
-    let spanMascotaJugador = document.getElementById('mascota-jugador');
+
+//CREAMOS VARIABLES DE CADA INPUT PARA USARLAS ENB LA CONDICINAL CON CHECKED
+//PARA DARLE MAS CLARIDAD AL CODIGO
+
+    let inputHipodoge = document.getElementById('hipodoge')
+    let inputCapipepo = document.getElementById('capipepo')
+    let inputRatigueya = document.getElementById('ratigueya')
+    let inputLangostelvi = document.getElementById('langostelvi')
+    let inputTucapalma = document.getElementById('tucapalma')
+    let inputPydos = document.getElementById('pydos')
+    let spanMascotaJugador = document.getElementById('mascota-jugador')
+
     
+//ABAJO CREAMOS UNA CONDICIONAL IF QUE DEPENDERÁ DEL INPUT SELECCIONADO
+//CON .CHECKED
+//SI EL INPUT SELECCIONADO ES CHECKEADO SERÁ TRUE Y HARA LO QUE LE PIDAMOS
+//EN ESTE CASO BOTAR ALERTA
+
+// LUEGO HACEMOS MODIFICACIÓN DEL DOM CON EL element.innerHTML para que
+//NUESTRA WEB COLOQUE EL TEXTO QUE INDICARÁ LA MASCOTA ELEGIDA
 
 
-    //MANIPULACIÓN DEL DOM CON innerHTML
-    if (inputHipodoge.checked) {
-        spanMascotaJugador.innerHTML = ' Hipodoge';
+
+
+// ----------------- JUGADOR ELIGE MASCOTA -----------------
+    if(inputHipodoge.checked) {
+    spanMascotaJugador.innerHTML = 'Hipodoge'
     } else if(inputCapipepo.checked) {
-        spanMascotaJugador.innerHTML = ' Capipepo';
-    } else if(inputRatigueya.checked) {
-        spanMascotaJugador.innerHTML = ' Ratigueya';
-    } else if(inputLangostelvi.checked) {
-        spanMascotaJugador.innerHTML = ' Langostelvi';
-    } else if(inputTucapalma.checked) {
-        spanMascotaJugador.innerHTML = ' Tucapalma';
-    } else if(inputPydos.checked) {
-        spanMascotaJugador.innerHTML = ' Pydos';
-    } else {
-        alert('DEBES SELECCIONAR UNA MASCOTA');
-    } 
-        
-    seleccionarMascotaEnemigo(lmml);
+        spanMascotaJugador.innerHTML = 'Capipepo'  
+    }else if(inputRatigueya.checked) {
+        spanMascotaJugador.innerHTML = 'Ratigueya'
+    }else if(inputLangostelvi.checked) {
+        spanMascotaJugador.innerHTML = 'Langostelvi'
+    }else if(inputTucapalma.checked) {
+        spanMascotaJugador.innerHTML = 'Tucapalma'
+    }else if(inputPydos.checked) {
+        spanMascotaJugador.innerHTML = 'Pydos'
+    }else {
+        alert('DEBES ELEGIR UNA MASCOTA PARA CONTINUAR')
 
-}
-
-function seleccionarMascotaEnemigo() {
-    let ataqueAleatorio = aleatorio(1,6);
-    let spanMascotaEnemigo = document.getElementById('mascota-enemigo');
-
-    if (ataqueAleatorio == 1) {
-        spanMascotaEnemigo.innerHTML = ' Hipodoge';
-    } else if (ataqueAleatorio == 2) {
-        spanMascotaEnemigo.innerHTML = ' Capipepo';
-    } else if (ataqueAleatorio == 3) {
-        spanMascotaEnemigo.innerHTML = ' Ratigueya';
-    } else if (ataqueAleatorio == 4) {
-        spanMascotaEnemigo.innerHTML = ' Langostelvi';
-    } else if (ataqueAleatorio == 5) {
-        spanMascotaEnemigo.innerHTML = ' Tucapalma';
-    } else {
-        spanMascotaEnemigo.innerHTML = ' Pydos';
     }
+    SeleccionarMascotaEnemigo()
+}
+
+// ----------------- ENEMIGO ELIGE MASCOTA -----------------
+function SeleccionarMascotaEnemigo() {
+let mascotaAleatoria = aleatorio(1,6)
+let spanMascotaEnemigo = document.getElementById('mascota-enemigo')
+if (mascotaAleatoria == 1){
+    spanMascotaEnemigo.innerHTML = 'Hipodogue'
+}else if (mascotaAleatoria == 2){
+    spanMascotaEnemigo.innerHTML = 'Capipepo'
+}else if (mascotaAleatoria == 3){
+    spanMascotaEnemigo.innerHTML = 'Ratigueya'
+}else if (mascotaAleatoria == 4){
+    spanMascotaEnemigo.innerHTML = 'Langostelvi'
+}else if (mascotaAleatoria == 5){
+    spanMascotaEnemigo.innerHTML = 'Tucapalma'
+}else{
+    spanMascotaEnemigo.innerHTML = 'Pydos'
+}
+}
+
+//FUNCIONES DE ATAQUES FUEGO, AGUA Y TIERRA
+
+function ataqueFuego(){
+    ataqueJugador = 'FUEGO'
+    ataqueAleatorioEnemigo()
+}
+
+function ataqueAgua(){
+    ataqueJugador = 'AGUA'
+    ataqueAleatorioEnemigo()
 
 }
 
+function ataqueTierra(){
+    ataqueJugador = 'TIERRA'
+    ataqueAleatorioEnemigo()
 
-//FUNCION PARA DARLE ALEATORIEDAD AL JS PARA QUE ESCOJA LA MASCOTA DEL ENEMIGO
+}
+
+//CREAMOS FUNCIÓN ATAQUE ENEMIGO, JUSTO DESPUES DEL ATAQUE JUGADOR
+
+function ataqueAleatorioEnemigo() {
+   let ataqueAleatorio = aleatorio(1,3)
+   
+   if (ataqueAleatorio == 1) {
+    ataqueEnemigo = 'FUEGO'
+   }else if (ataqueAleatorio == 2) {
+    ataqueEnemigo = 'AGUA'
+   }else{
+    ataqueEnemigo = 'TIERRA'
+   }
+
+
+  combate() 
+
+}
+
+function combate() {
+
+
+if (ataqueEnemigo == ataqueJugador) {
+    crearMensaje("ES EMPATE");
+    
+    } else if (ataqueJugador == 'FUEGO' && ataqueEnemigo == 'TIERRA' || ataqueJugador == 'AGUA' && ataqueEnemigo == 'FUEGO' || ataqueJugador == 'TIERRA' && ataqueEnemigo == 'AGUA' ) { 
+    crearMensaje("GANASTE");
+    
+    }  else {
+    crearMensaje("PERDISTE");
+    }
+}
+//IMPORTANTE ------- CREAMOS LA FUNCIÓN CREAR MENSAJES
+//PARA INSERTAR UN TEXTO EN HTML CREADO MEDIABTE document.createElement,  
+//INSERTADO POR appendChild Y DIRIGIDO POR getElementById
+
+function crearMensaje(resultado) {
+    let secctionMensajes = document.getElementById('mensajes')
+
+
+    let parrafo = document.createElement('p')
+    parrafo.innerHTML = 'Tu mascota atacó con ' + ataqueJugador + ' y la mascota de tu enemigo atacó con ' + ataqueEnemigo + ': ' + resultado
+
+    secctionMensajes.appendChild(parrafo)
+    
+}
+
+
+//FUNCION DE ALEATORIEDAD PARA QUE ELIJA MASCOTA POR EL ENEMIGO
 
 function aleatorio (min, max) {
     return Math.floor(Math.random() * (max - min + 1) + min);
@@ -67,9 +179,5 @@ function aleatorio (min, max) {
 
 
 
-
-// CODIGO LOAD QUE SE ACTIVA CUANDO SE HA CARGADO TODA LA PAGINA Y TODOS SUS RECURSOS
-//PERMITE TRABAJAR CON EL RECURSO JS AL PRICIPIO DEL HTML EN EL HEAD
-window.addEventListener('load', iniciarJuego)
 
 
